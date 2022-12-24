26f800d065387495b0785047470d3a1d79c9affa
tcaminal <taminalros@gmail.com>
2022-12-24 13:39:52 +0100

    asd

---
+++ Project.c
@@ -0,0 +1,15 @@
+/*
+Autors: 			Marcel Monereo, Javier Bustos, 
+							Alexander Jadnov, Tomàs Caminal
+Data: 				24/12/2022
+Assignatura:	IIEIA
+*/
+
+#include <reg51.h>
+
+//Declaración de variables
+
+
+void main(void)
+{
+}
