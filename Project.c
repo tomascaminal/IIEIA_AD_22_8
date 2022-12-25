c1ef7848c96a9186e93db4b3980e6bb998d0c351
retuif <121357922+retuif@users.noreply.github.com>
2022-12-25 11:51:21 +0100

    Modified "Project.c"

--- Project.c
+++ Project.c
@@ -3,12 +3,14 @@ Autors: 			Marcel Monereo, Javier Bustos,
 							Alexander Jadnov, Tomàs Caminal
 Data: 				24/12/2022
 Assignatura:	IIEIA
-test 2
 */
 
 #include <reg51.h>
-<
-//Declaración de variables
+sbit WR = P3^0;
+sbit INTR = P3^1;
+
+#define LEDs = P2;
+//Declaració de variables
 
 
 void main(void)
